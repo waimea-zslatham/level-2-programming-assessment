d96291996e7aa7f2ec6a8772aade637f1b8a8bd7
waimea-zslatham <zslatham@waimea.school.nz>
2025-03-24 10:48:00 +1300

    title and how to play

--- src/Main.kt
+++ src/Main.kt
@@ -14,5 +14,26 @@
 
 
 fun main() {
-    println("Hello World!")
+    println(" ________  ________  _______   _______   ________      ___    ___ \n" +
+            "|\\   ____\\|\\   __  \\|\\  ___ \\ |\\  ___ \\ |\\   ___ \\    |\\  \\  /  /|\n" +
+            "\\ \\  \\___|\\ \\  \\|\\  \\ \\   __/|\\ \\   __/|\\ \\  \\_|\\ \\   \\ \\  \\/  / /\n" +
+            " \\ \\  \\  __\\ \\   _  _\\ \\  \\_|/_\\ \\  \\_|/_\\ \\  \\ \\\\ \\   \\ \\    / / \n" +
+            "  \\ \\  \\|\\  \\ \\  \\\\  \\\\ \\  \\_|\\ \\ \\  \\_|\\ \\ \\  \\_\\\\ \\   \\/  /  /  \n" +
+            "   \\ \\_______\\ \\__\\\\ _\\\\ \\_______\\ \\_______\\ \\_______\\__/  / /    \n" +
+            "    \\|_______|\\|__|\\|__|\\|_______|\\|_______|\\|_______|\\___/ /     \n" +
+            " ________  ___  ________                             \\|___|/      \n" +
+            "|\\   __  \\|\\  \\|\\   ____\\                                         \n" +
+            "\\ \\  \\|\\  \\ \\  \\ \\  \\___|                                         \n" +
+            " \\ \\   ____\\ \\  \\ \\  \\  ___                                       \n" +
+            "  \\ \\  \\___|\\ \\  \\ \\  \\|\\  \\                                      \n" +
+            "   \\ \\__\\    \\ \\__\\ \\_______\\                                     \n" +
+            "    \\|__|     \\|__|\\|_______|                                     ")
+
+    println("-----------------------------------------------------------------------------------------------------")
+    println("                                 HOW TO PLAY")
+    println("Greedy Pig is a two-player game, played with a Die but on a computer.")
+    println("It will randomly generate a number from 1 to 6 and you have to keep score on your grid.")
+    println("If it lands on a 1 you lose all your points from that round.")
+    println("You can chose to sit out if you think you have enough from that round.")
+    println("Whoever has the most at the end of each round wins.")
 }
