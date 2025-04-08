e6e380b636b0a9bfdbee86f19200241be665198f
waimea-zslatham <zslatham@waimea.school.nz>
2025-04-08 15:07:27 +1200

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -14,7 +14,13 @@
 
 
 fun main() {
-
+    val action = menu()
+    when (action) {
+        'a' -> startGame()
+        'b' -> tutorial()
+        'c' -> endGame()
+    }
+}
 //    dice.add("1")
 //    dice.add("2")
 //    dice.add("3")
@@ -30,7 +36,7 @@ fun main() {
 
 
 
-
+fun title() {
     println(
         "  _______ _            _____  _             _____                      \n" +
                 "|__   __| |          |  __ \\(_)           / ____|                     \n" +
@@ -48,41 +54,38 @@ fun main() {
     println("You can chose to sit out if you think you have enough from that round.")
     println("Whoever has the most at the end of each round wins.")
     println()
-
-    val randomNumber = (1..12).random()
-    println(randomNumber)
-
-    fun menu(): Char {
-        println("[A] PLAY")
+}
+    fun menu() : Char {
+        println("[a] PLAY")
         println("===============")
-        println("[B] TUTORIAL ON HOW TO PLAY")
+        println("[b] TUTORIAL ON HOW TO PLAY")
         println("================================")
-        println("[C] QUIT")
+        println("[c] QUIT")
         println("=============")
 
-        val validChoice = "ABCD"
+        val validChoice = "abc"
 
         while (true) {
             print("Choice: ")
+
             val input = readln()
             // Typed nothing? Try again!
             if (input.isBlank()) continue
             // Grab the first letter
-            val choice = input.uppercase().first()
+            val choice = input.lowercase().first()
             // Check it is a valid option
             if (validChoice.contains(choice)) return choice
-            when (choice) {
-                'A' -> startGame
-                'B' -> tutorial
-//            'D' -> deletePet()
-            }
         }
+    }
 
         // The function that starts the game from the A choice
-        fun startGame() {
+            fun startGame(): Char {
+            val player1 = getString("Please enter your name Player 1: ")
+            val player2 = getString("Please enter your name Player 2: ")
+            println("Welcome to The Dice Game $player1 and $player2")
 
-
-        }
+            val randomNumber = (1..12).random()
+            println(randomNumber)
 
         //The function that gives the player information on how to play (tutorial)
         fun tutorial() {
@@ -90,15 +93,11 @@ fun main() {
         }
 
         // The function that ends the game for the user. (Breaks loop)
-        fun endGame() {
+        fun endGame() : Char {
             println("==================================================")
-            println("Are you sure you would like to leave the game?")
+            println("Thanks for playing the Dice Game")
             println("==================================================")
-            println("[Y] YES")
-            println("[N] NO")
-            val validChoice = "YN"
-            while (true) {
-                print("Choice: ")
+            return 'a'
             }
 
 //        val input = readln() {
@@ -117,7 +116,19 @@ fun main() {
 //                'N' ->
 //            }
         }
+
+fun getString (prompt: String) : String {
+    var userInput: String
+
+    while(true) {
+        print(prompt)
+
+        userInput = readln()
+        if (userInput.isBlank()) break
     }
+
+    return userInput
+}
 
 
 
